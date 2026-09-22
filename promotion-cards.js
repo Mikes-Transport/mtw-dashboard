bcef6d2e0dbb56c0a46d3beef180dc97507322df
Mikes-Transport <mtwaustin@gmail.com>
2026-09-23 11:20:20 +1200

    Update promotion-cards.js

--- promotion-cards.js
+++ promotion-cards.js
@@ -3387,9 +3387,9 @@ overscroll-behavior-y:contain!important;
 .db-left-content.editing .et-edit-panel{
 flex:1 1 auto!important;
 width:100%!important;
-height: 100% !important;
+height: 50% !important;
 min-height:0!important;
-max-height:none!important;
+max-height: 50% !important;
 overflow-y:auto!important;
 overflow-x:hidden!important;
 box-sizing:border-box!important;
